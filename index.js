403b55e9ead37d8b05d22fbbba60e74e9e0092de
John Azzaro <jazzaro@temple.edu>
2019-09-26 08:10:30 -0700

    improved js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 "use strict";
 
-const hamburger = document.querySelector('.hamburger');         // selects hamburger icon.
-const navLinks = document.querySelector('.nav-links');          // selects nav-links element
-const links = document.querySelectorAll(".nav-links li");       // selects all the li's in nav-links
+function burgerClick() {
+    const hamburger = document.querySelector('.hamburger');         // selects hamburger icon.
+    const navLinks = document.querySelector('.nav-links');          // selects nav-links element
+    const links = document.querySelectorAll(".nav-links li");       // selects all the li's in nav-links
 
-hamburger.addEventListener("click", function(event) {            // on click of the hamburger icon...
-    navLinks.classList.toggle('open');                           // toggle the "open" style
-    links.forEach(function(link) {
-        link.classList.toggle('fade');
+    hamburger.addEventListener("click", function(event) {            // on click of the hamburger icon...
+        navLinks.classList.toggle('open');                           // toggle the "open" style
+        links.forEach(function(link) {
+            link.classList.toggle('fade');
+        });
     });
-});
+}
 
+function setupEventHandlers() {
+    burgerClick();
+}
+
+function initialize() {
+    setupEventHandlers();
+}
+
+$(initialize);
