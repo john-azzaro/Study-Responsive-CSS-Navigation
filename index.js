d31144dce0637176228bf093733a74017a271b08
John Azzaro <jazzaro@temple.edu>
2019-09-25 21:38:46 -0700

    added link animation, js toggle, notes

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const hamburger = document.querySelector('.hamburger');         // selects hambu
 const navLinks = document.querySelector('.nav-links');          // selects nav-links element
 const links = document.querySelectorAll(".nav-links li");       // selects all the li's in nav-links
 
-
-
+hamburger.addEventListener("click", (event) => {                 // on click of the hamburger icon...
+    navLinks.classList.toggle('open');                           // toggle the "open" style
+    links.forEach(link => {
+        link.classList.toggle('fade');
+    });
+});
 
