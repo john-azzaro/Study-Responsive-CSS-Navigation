930c09afe768985dd773747072aba6b65d420497
John Azzaro <jazzaro@temple.edu>
2019-09-26 08:07:59 -0700

    improved js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const hamburger = document.querySelector('.hamburger');         // selects hambu
 const navLinks = document.querySelector('.nav-links');          // selects nav-links element
 const links = document.querySelectorAll(".nav-links li");       // selects all the li's in nav-links
 
-hamburger.addEventListener("click", (event) => {                 // on click of the hamburger icon...
+hamburger.addEventListener("click", function(event) {            // on click of the hamburger icon...
     navLinks.classList.toggle('open');                           // toggle the "open" style
-    links.forEach(link => {
+    links.forEach(function(link) {
         link.classList.toggle('fade');
     });
 });
