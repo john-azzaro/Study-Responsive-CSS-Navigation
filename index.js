071945cca9f0d7a4b97c37a5f9890275a1f35fe1
John Azzaro <jazzaro@temple.edu>
2019-09-25 21:14:02 -0700

    selectors

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 "use strict";
 
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector('.hamburger');         // selects hamburger icon.
+const navLinks = document.querySelector('.nav-links');          // selects nav-links element
+const links = document.querySelectorAll(".nav-links li");       // selects all the li's in nav-links
+
+
+
+
